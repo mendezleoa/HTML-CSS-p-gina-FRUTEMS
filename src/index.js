080c17311c81d080be766d96bc0b131998fdc9b5
mendezleoa <92312161+mendezleoa@users.noreply.github.com>
2023-06-29 00:18:46 -0400

    Integracion de webpack con CSS loader

---
+++ src/index.js
@@ -0,0 +1,3 @@
+console.log('Compilando el CSS con Webpack 5');
+
+import './style.css';
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: "file-loader",
+      },
+      {
+        test: /\.png$/,
+        type: 'asset/resource'
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
+};
